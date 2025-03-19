99d9907a9ad7bfec36be40685e91a16f16799c34
Kroii04 <111943348+Kroii04@users.noreply.github.com>
2025-03-19 11:39:15 +0800

    Update who_is_it.js

--- js/who_is_it.js
+++ js/who_is_it.js
@@ -6,7 +6,7 @@ let location_sh = document.querySelector('.location_sh')
 let csdn = document.querySelector('.csdn')
 let leetcode = document.querySelector('.leetcode')
 
-if (a === 'nbnd6v0x4') {
+if (a === 'your_password') {
     introduction_downloadCV_button.setAttribute('href', 'resource/cv.pdf');
     download_demo_button.setAttribute('href', 'resource/MyWebSite.zip')
 
@@ -14,8 +14,8 @@ if (a === 'nbnd6v0x4') {
     e_mail_self.innerHTML = 'gwkuroii@gmail.com'
     location_sh.innerHTML = 'ShangHai,CNH'
 
-    csdn.setAttribute('href', 'https://blog.csdn.net/m0_73642416?spm=1000.2115.3001.5343')
-    leetcode.setAttribute('href', 'https://leetcode.cn/u/ku_roii/')
+    csdn.setAttribute('href', '#')
+    leetcode.setAttribute('href', '#')
 } else {
     introduction_downloadCV_button.setAttribute('href', '#');
     download_demo_button.setAttribute('href', '#')
